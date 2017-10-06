6d63086b7004621f69bf80bb5046ebb6d543d99d
David Weis <dweis7@gmail.com>
2017-10-06 10:15:36 +0200

    Add method for pulling data

--- app.js
+++ app.js
@@ -13,16 +13,53 @@ class Person {
     }
 
     toString() {
-        return `${this.name.toString()} ${this.email} ${this.phone} ${this.address.toString()} ${this.nationality} ${this.salary}`
+        return '${this.name.toString()} ${this.email} ${this.phone} ${this.address.toString()} ${this.nationality} ${this.salary}'
     }
 }
 
+class Name {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    toString() {
+        return '${this.firstName.charAt(0).toUpperCase() + this.firstName.slice(1)} ${this.lastName.charAt(0).toUpperCase() + this.lastName.slice(1)}'
+    }
+}
+
+class Location {
+    constructor(city, postcode, state, street) {
+        this.city = city;
+        this.postcode = postcode;
+        this.state = state;
+        this.street = street;
+    }
+
+    toString() {
+        return '${this.postcode}, ${this.city}, ${this.state}, ${this.street}'
+    }
+}
+
+function randomUserToPerson(user) {
+    var salary = (parseInt(Math.floor(Math.random() * 40) + 10)) * 1000;
+    var name = new Name(user.name.first, user.name.last);
+    var location = new Location(user.location.city, user.location.postcode, user.location.state, user.location.street)
+    var person = new Person(user.login.username, name, user.email, user.phone, location, user.picture.large, user.nat, salary);
+    return person;
+}
+
 employeeListApp.controller('EmployeeListController', function EmployeeListController($scope) {
-  $scope.employees = function getEmployees(){
-      var user = new Person(6, 'David', "sadas", "asdas", "waht ever", "http://i0.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg", "asdasd", 65000);
-      var data = [];
-      data.push(user);
-      console.log(data);
-      return data;
-  }
+    $scope.employees = [];
+    $scope.LoadData = function () {
+        $.ajax({
+            url: 'https://randomuser.me/api/?nat=dk,gb,us&inc=name,phone,email,location,picture,login,nat&results=20',
+            dataType: 'json',
+            success: function (r) {
+                var data = r.results.map(randomUserToPerson);
+                $scope.employees = data;
+                $scope.$apply();
+            }
+        });
+    }
 });
